adeb8cd87cc88b82b446b7d8f93b300664ad25d4
Theo <theo.farnole@gmail.com>
2019-09-21 17:52:55 +0200

    Clean project

---
+++ Assets/Scripts/Game/Registers/BuildingsPortraitsRegister.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingsPortraitsRegister : Register<Sprite, Building>
+{
+    [EnumNamedArray(typeof(Building))]
+    [SerializeField] private Sprite[] _buildingsPortraits;
+
+    protected override Sprite[] Prefabs { get => _buildingsPortraits; set => _buildingsPortraits = value; }
+}
---
+++ Assets/Scripts/Game/Registers/UnitsPortraitsRegister.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Unit
+{
+    Alexios,
+    Kassandra
+}
+
+public class UnitsPortraitsRegister : Register<Sprite, Unit>
+{
+    [EnumNamedArray(typeof(Unit))]
+    [SerializeField] private Sprite[] _unitsPortraits;
+
+    protected override Sprite[] Prefabs { get => _unitsPortraits; set => _unitsPortraits = value; }
+}
