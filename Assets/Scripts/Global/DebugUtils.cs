af63828a621f920820249935006f77f28736991a
Theo <theo.farnole@gmail.com>
2019-09-30 18:45:08 +0200

    Minors debug changement

--- Assets/Scripts/Global/DebugUtils.cs
+++ Assets/Scripts/Global/DebugUtils.cs
@@ -6,8 +6,13 @@ using UnityEngine;
 public class DebugUtils : MonoBehaviour
 {
     #region Fields
+    [Header("Activation Settings")]
     [SerializeField] private bool _activeOnStart = false;
-    [SerializeField] private KeyCode _toggleKey;
+    [SerializeField] private KeyCode _toggleKey = KeyCode.F2;
+    [Header("Give Resources Settings")]
+    [SerializeField] private KeyCode _giveKey = KeyCode.F3;
+    [SerializeField] private ResourcesWrapper _giveResources = new ResourcesWrapper(100, 100, 100);
+
     private bool _active = true;
     #endregion
 
@@ -27,6 +32,14 @@ public class DebugUtils : MonoBehaviour
         if (Input.GetKeyDown(_toggleKey))
         {
             _active = !_active;
+        }
+
+        if (_active)
+        {
+            if (Input.GetKeyDown(_giveKey))
+            {
+                GameManager.Instance.Resources += _giveResources;
+            }
         }
     }
 
