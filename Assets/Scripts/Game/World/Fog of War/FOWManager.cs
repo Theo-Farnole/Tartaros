dafa2e80194dc2836088143223fdd98857094c6c
Theo <theo.farnole@gmail.com>
2019-10-15 20:25:32 +0200

    Improve security of FOWManager visibility map

--- Assets/Scripts/Game/World/Fog of War/FOWManager.cs
+++ Assets/Scripts/Game/World/Fog of War/FOWManager.cs
@@ -7,17 +7,9 @@ namespace FogOfWar
 {
     public class FOWManager : Singleton<FOWManager>
     {
-        #region Struct
-        
-        #endregion
-
         #region Fields
-        public static readonly int NOT_VISIBLE = 0;
-        public static readonly int REVEALED = 1;
-        public static readonly int VISIBLE = 2;
-
         [SerializeField] private SnapGrid _snapGrid;
-        private int[,] _visiblityMap;
+        private FogState[,] _visiblityMap;
 
         private List<FOWEntity> _viewers = new List<FOWEntity>();
         private List<FOWEntity> _coverables = new List<FOWEntity>();
@@ -27,14 +19,14 @@ namespace FogOfWar
         #region MonoBehaviour Callbacks
         void Awake()
         {
-            _visiblityMap = new int[_snapGrid.Data.CellCount, _snapGrid.Data.CellCount];
+            _visiblityMap = new FogState[_snapGrid.Data.CellCount, _snapGrid.Data.CellCount];
 
             // initialize circle with NOT_VISIBLE
             for (int i = 0; i < _visiblityMap.GetLength(0); i++)
             {
                 for (int j = 0; j < _visiblityMap.GetLength(1); j++)
                 {
-                    _visiblityMap[i, j] = NOT_VISIBLE;
+                    _visiblityMap[i, j] = FogState.NotVisible;
                 }
             }
         }
@@ -84,9 +76,9 @@ namespace FogOfWar
             {
                 for (int y = 0; y < _visiblityMap.GetLength(1); y++)
                 {
-                    if (_visiblityMap[x, y] == VISIBLE)
+                    if (_visiblityMap[x, y] == FogState.Visible)
                     {
-                        _visiblityMap[x, y] = REVEALED;
+                        _visiblityMap[x, y] = FogState.Revealed;
                     }
                 }
             }
@@ -97,7 +89,7 @@ namespace FogOfWar
                 Vector2Int viewersCoords = _snapGrid.GetNearestCoords(_viewers[i].transform.position);
                 int viewRadius = Mathf.RoundToInt(_viewers[i].ViewRadius / _snapGrid.Data.CellSize);
 
-                _visiblityMap.DrawCircleInside(viewersCoords.x, viewersCoords.y, viewRadius, VISIBLE);
+                _visiblityMap.DrawCircleInside(viewersCoords.x, viewersCoords.y, viewRadius, FogState.Visible);
             }
         }
 
@@ -112,7 +104,7 @@ namespace FogOfWar
                 if (coords.x >= 0 && coords.x < _visiblityMap.GetLength(0) &&
                     coords.y >= 0 && coords.y < _visiblityMap.GetLength(1))
                 {
-                    if (_visiblityMap[coords.x, coords.y] == VISIBLE)
+                    if (_visiblityMap[coords.x, coords.y] == FogState.Visible)
                     {
                         isCover = false;
                     }
@@ -130,9 +122,9 @@ namespace FogOfWar
             {
                 for (int x = 0; x < _visiblityMap.GetLength(0); x++)
                 {
-                    if (_visiblityMap[x, y] == NOT_VISIBLE) sb.Append(".");
-                    else if (_visiblityMap[x, y] == REVEALED) sb.Append("_");
-                    else if (_visiblityMap[x, y] == VISIBLE) sb.Append("X");
+                    if (_visiblityMap[x, y] == FogState.NotVisible) sb.Append(".");
+                    else if (_visiblityMap[x, y] == FogState.Revealed) sb.Append("_");
+                    else if (_visiblityMap[x, y] == FogState.Visible) sb.Append("X");
                 }
 
                 sb.AppendLine();
---
+++ Assets/Scripts/Game/World/Fog of War/FogState.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FogOfWar
+{
+    public enum FogState
+    {
+        NotVisible = 0,
+        Revealed = 1,
+        Visible = 2
+    }
+
+    public static class FogStateExtension
+    {
+        // code from
+        // https://rosettacode.org/wiki/Bitmap/Midpoint_circle_algorithm#C.23k
+        //
+        public static void DrawCircleInside(this FogState[,] array, int centerX, int centerY, int radius, FogState fulfillValue)
+        {
+            int d = (5 - radius * 4) / 4;
+            int x = 0;
+            int y = radius;
+
+            do
+            {
+                // ensure index is in range before setting (depends on your image implementation)
+                // in this case we check if the pixel location is within the bounds of the image before setting the pixel
+                if (centerX + x >= 0 && centerX + x <= array.GetLength(0) - 1 && centerY + y >= 0 && centerY + y <= array.GetLength(1) - 1 &&
+                    centerX + x >= 0 && centerX + x <= array.GetLength(0) - 1 && centerY - y >= 0 && centerY - y <= array.GetLength(1) - 1)
+                {
+                    DrawLine(array, centerX + x, centerY + y, centerX + x, centerY - y, fulfillValue);
+                }
+
+                if (centerX - x >= 0 && centerX - x <= array.GetLength(0) - 1 && centerY + y >= 0 && centerY + y <= array.GetLength(1) - 1 &&
+                    centerX - x >= 0 && centerX - x <= array.GetLength(0) - 1 && centerY - y >= 0 && centerY - y <= array.GetLength(1) - 1)
+                {
+                    DrawLine(array, centerX - x, centerY - y, centerX - x, centerY + y, fulfillValue);
+                }
+
+                if (centerX + y >= 0 && centerX + y <= array.GetLength(0) - 1 && centerY + x >= 0 && centerY + x <= array.GetLength(1) - 1 &&
+                    centerX + y >= 0 && centerX + y <= array.GetLength(0) - 1 && centerY - x >= 0 && centerY - x <= array.GetLength(1) - 1)
+                {
+                    DrawLine(array, centerX + y, centerY + x, centerX + y, centerY - x, fulfillValue);
+                }
+
+                if (centerX - y >= 0 && centerX - y <= array.GetLength(0) - 1 && centerY + x >= 0 && centerY + x <= array.GetLength(1) - 1 &&
+                    centerX - y >= 0 && centerX - y <= array.GetLength(0) - 1 && centerY - x >= 0 && centerY - x <= array.GetLength(1) - 1)
+                {
+                    DrawLine(array, centerX - y, centerY + x, centerX - y, centerY - x, fulfillValue);
+                }
+
+                if (d < 0)
+                {
+                    d += 2 * x + 1;
+                }
+                else
+                {
+                    d += 2 * (x - y) + 1;
+                    y--;
+                }
+                x++;
+            } while (x <= y);
+        }
+
+        // code from
+        // https://rosettacode.org/wiki/Bitmap/Bresenham%27s_line_algorithm#C.23
+        public static void DrawLine(this FogState[,] array, int x0, int y0, int x1, int y1, FogState fulfillValue)
+        {
+            int dx = Mathf.Abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
+            int dy = Mathf.Abs(y1 - y0), sy = y0 < y1 ? 1 : -1;
+            int err = (dx > dy ? dx : -dy) / 2, e2;
+            for (; ; )
+            {
+                array[x0, y0] = fulfillValue;
+                if (x0 == x1 && y0 == y1) break;
+                e2 = err;
+                if (e2 > -dx) { err -= dy; x0 += sx; }
+                if (e2 < dy) { err += dx; y0 += sy; }
+            }
+        }
+    }
+}
