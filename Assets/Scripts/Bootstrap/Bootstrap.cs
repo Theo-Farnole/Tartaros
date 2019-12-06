e5cbbc8f346b970bf4000ad6892203fe14a8532f
Theo <theo.farnole@gmail.com>
2019-12-06 22:19:27 +0100

    Make project build-able

---
+++ Assets/Scripts/Bootstrap/Bootstrap.cs
@@ -0,0 +1,27 @@
+﻿using Lortedo.Utilities.Managers;
+using MyBox;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Bootstrap : MonoBehaviour
+{
+#if UNITY_EDITOR
+    [SerializeField] private SceneAsset _sceneToLoad;
+#endif
+
+    [SerializeField, ReadOnly] private string _sceneToLoadName;
+
+    void Start()
+    {
+        SceneManager.LoadScene(_sceneToLoadName);
+    }
+
+#if UNITY_EDITOR
+    void OnValidate()
+    {
+        _sceneToLoadName = _sceneToLoad.name;
+    }
+#endif
+}
--- Assets/__Addons/Draw If Attribute/SerializedPropertyExtentions.cs
+++ Assets/__Addons/Draw If Attribute/SerializedPropertyExtentions.cs
@@ -2,6 +2,7 @@
 
 namespace Utilities
 {
+#if UNITY_EDITOR
     public static class SerializedPropertyExtentions
     {
 	    public static T GetValue<T>(this SerializedProperty property)
@@ -9,4 +10,5 @@ namespace Utilities
             return ReflectionUtil.GetNestedObject<T>(property.serializedObject.targetObject, property.propertyPath);
         }
     }
+#endif
 }
