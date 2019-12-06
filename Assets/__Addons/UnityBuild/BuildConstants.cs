e92bbac75514459960e52a13e6f442843352f04a
Theo <theo.farnole@gmail.com>
2019-12-06 22:06:21 +0100

    Add Addon: UnityBuild
    Make easier builds naming

---
+++ Assets/__Addons/UnityBuild/BuildConstants.cs
@@ -0,0 +1,35 @@
+// This file is auto-generated. Do not modify or move this file.
+
+public static class BuildConstants
+{
+    public enum ReleaseType
+    {
+        None,
+        release,
+    }
+
+    public enum Platform
+    {
+        None,
+        PC,
+    }
+
+    public enum Architecture
+    {
+        None,
+        Windows_x64,
+    }
+
+    public enum Distribution
+    {
+        None,
+    }
+
+    public static readonly System.DateTime buildDate = new System.DateTime(637096851103150796);
+    public const string version = "alpha1.0.1b4_day12";
+    public const ReleaseType releaseType = ReleaseType.release;
+    public const Platform platform = Platform.PC;
+    public const Architecture architecture = Architecture.Windows_x64;
+    public const Distribution distribution = Distribution.None;
+}
+
