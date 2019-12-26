9e03ef21ee107d7ef5ceb44500cff3d4e53ba5ea
Theo <theo.farnole@gmail.com>
2019-12-26 21:45:49 +0100

    Removing unused singleton

--- Assets/Scripts/Game/Managers/InputsListener/HotkeyActionListener.cs
+++ Assets/Scripts/Game/Managers/InputsListener/HotkeyActionListener.cs
@@ -6,7 +6,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HotkeyActionListener : Singleton<HotkeyActionListener>
+public class HotkeyActionListener : MonoBehaviour
 {
     public enum AskCursor
     {
