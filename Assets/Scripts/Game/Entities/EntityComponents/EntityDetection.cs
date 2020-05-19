d71c49f18186522e1e84b7573dbe137ced426d4d
Theo <theo.farnole@gmail.com>
2020-05-19 12:19:30 +0200

    Performance: [entity] EntityDetection GetAllEnemiesInViewRadius improve
    20 FPS to 120FPS

--- Assets/Scripts/Game/Entities/EntityComponents/EntityDetection.cs
+++ Assets/Scripts/Game/Entities/EntityComponents/EntityDetection.cs
@@ -1,17 +1,85 @@
-﻿using System.Collections;
+﻿using Sirenix.OdinInspector;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class EntityDetection : EntityComponent
 {
+    private const string debugLogHeader = "Entity Detection : ";
     public readonly static float DISTANCE_THRESHOLD = 0.3f;
 
+    [Required]
+    [SerializeField] private GenericTrigger _viewTrigger;
+
+    private List<Entity> _enemiesInViewRadius = new List<Entity>();
+
+    #region Methods
+    #region MonoBehaviour Callbacks
+    void OnEnable()
+    {
+        if (_viewTrigger != null)
+        {
+            _viewTrigger.enabled = true;
+            _viewTrigger.OnTriggerEnterEvent += GenericTrigger_OnTriggerEnterEvent;
+            _viewTrigger.OnTriggerExitEvent += GenericTrigger_OnTriggerExitEvent;
+        }
+        else
+        {
+            Debug.LogErrorFormat(debugLogHeader + "Please assign a _genericTrigger to {0}", name);
+        }
+
+        _enemiesInViewRadius.Clear();
+    }
+
+    void OnDisable()
+    {
+        if (_viewTrigger != null)
+        {
+            _viewTrigger.enabled = false;
+            _viewTrigger.OnTriggerEnterEvent -= GenericTrigger_OnTriggerEnterEvent;
+            _viewTrigger.OnTriggerExitEvent -= GenericTrigger_OnTriggerExitEvent;
+        }
+    }
+    #endregion
+
+    #region Events handlers
+    private void GenericTrigger_OnTriggerExitEvent(Collider other)
+    {
+        if (other.gameObject.TryGetComponent(out Entity entity))
+        {
+            if (entity.Team != Entity.Team)
+            {
+                Assert.IsFalse(_enemiesInViewRadius.Contains(entity), string.Format(debugLogHeader + "entity {0} is already in {1} list.", name, nameof(_enemiesInViewRadius)));
+                _enemiesInViewRadius.Add(entity);
+            }
+        }
+    }
+
+    private void GenericTrigger_OnTriggerEnterEvent(Collider other)
+    {
+        if (other.gameObject.TryGetComponent(out Entity entity))
+        {
+            // the enemy shoulddn't has the same team of the Entity
+            Assert.IsFalse(_enemiesInViewRadius.Contains(entity) && entity.Team == Entity.Team,
+                string.Format(debugLogHeader + "The entity {0} in {1} has the same team of {2}'s entity detection.", entity.name, nameof(_enemiesInViewRadius), name));
+
+            if (entity.Team != Entity.Team && _enemiesInViewRadius.Contains(entity))
+            {
+                _enemiesInViewRadius.Remove(entity);
+            }
+        }
+    }
+    #endregion
+
+
+    #region Public methods
     public bool IsEntityInAttackRange(Entity target)
     {
         return Vector3.Distance(transform.position, target.transform.position) <= Entity.Data.AttackRadius;
     }
-    
+
     public bool IsNearFromEntity(Entity target)
     {
         if (Vector3.Distance(transform.position, target.transform.position) <= DISTANCE_THRESHOLD)
@@ -42,16 +110,13 @@ public class EntityDetection : EntityComponent
     /// <returns></returns>
     public Entity[] GetAllEnemiesInViewRadius()
     {
-        var entityTeam = Entity.Team;
-        
-        var overlapCollision = Physics.OverlapSphere(transform.position, Entity.Data.ViewRadius);
+        if (!enabled)
+        {
+            Debug.LogWarningFormat(debugLogHeader + " {0} is disabled. Can't get all enemies in view radius.");
+            return null;
+        }
 
-        var enemiesOutput = overlapCollision
-            .Select(x => x.GetComponent<Entity>()) // get all components
-            .Where(x => x != null && x.Team != entityTeam) // remove allies & null components
-            .ToArray();
-
-        return enemiesOutput;
+        return _enemiesInViewRadius.ToArray();
     }
 
     /// <summary>
@@ -77,4 +142,6 @@ public class EntityDetection : EntityComponent
     {
         return Vector3.Distance(transform.position, target.transform.position) <= Entity.Data.ViewRadius;
     }
+    #endregion
+    #endregion
 }
