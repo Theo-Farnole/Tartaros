1c2c786b467b29514ff62e452d45adb8b9f62d57
Theo <theo.farnole@gmail.com>
2020-05-19 12:18:56 +0200

    Add: [utilities] Generic Trigger (events OnTriggerEnter and OnTriggerExit)

---
+++ Assets/Scripts/Utilities/Specific/GenericTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void OnTriggerEnter(Collider other);
+public delegate void OnTriggerExit(Collider other);
+
+public class GenericTrigger : MonoBehaviour
+{
+    public event OnTriggerEnter OnTriggerEnterEvent;
+    public event OnTriggerExit OnTriggerExitEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        OnTriggerEnterEvent?.Invoke(other);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        OnTriggerExitEvent?.Invoke(other);
+    }
+}
