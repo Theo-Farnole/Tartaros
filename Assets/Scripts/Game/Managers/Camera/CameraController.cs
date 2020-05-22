dbabc178496f2a2ac523edbedda25e5f9363a55c
Theo <theo.farnole@gmail.com>
2020-05-22 09:35:30 +0200

    Add: [camera] set movement relative to camera's orientation

--- Assets/Scripts/Game/Managers/Camera/CameraController.cs
+++ Assets/Scripts/Game/Managers/Camera/CameraController.cs
@@ -1,16 +1,36 @@
-﻿using System.Collections;
+﻿using Sirenix.OdinInspector;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private float _speed = 3;
+    [Required]
+    [SerializeField] private CameraControllerData _data;
 
     void Update()
     {
+        ProcessMovement(Time.deltaTime);
+    }
+
+    private void ProcessMovement(float deltaTime)
+    {
+        Assert.IsNotNull(_data, "Please assign a CameraControllerData to camera " + name + ".");
+
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        transform.position += new Vector3(horizontal, 0, vertical) * _speed * Time.deltaTime;
+        Vector3 forward = transform.forward;
+        Vector3 right = transform.right;
+
+        // project 'forward' and 'right' on plane
+        forward.y = 0;
+        right.y = 0;
+
+        Vector3 deltaForward = _data.Speed * deltaTime * vertical * forward;
+        Vector3 deltaRight = _data.Speed * deltaTime * horizontal * right;
+
+        transform.position += deltaForward + deltaRight;
     }
 }
---
+++ Assets/Scripts/Game/Managers/Camera/CameraControllerData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Tartaros/Camera Controller")]
+public class CameraControllerData : ScriptableObject
+{
+    [SerializeField] private float _speed = 3;
+
+    public float Speed { get => _speed; }
+}
