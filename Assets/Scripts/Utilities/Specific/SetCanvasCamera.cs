ce81aeb5d291839c6dd934a705cb99c89ad37c9c
Theo <theo.farnole@gmail.com>
2020-05-26 16:39:11 +0200

    Add + Performance: [entity] world bar always look at camera + set event camera at start

---
+++ Assets/Scripts/Utilities/Specific/SetCanvasCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+[RequireComponent(typeof(Canvas))]
+public class SetCanvasCamera : MonoBehaviour
+{    
+    void Start()
+    {
+        var canvas = GetComponent<Canvas>();
+
+        Assert.IsNotNull(canvas);
+
+        canvas.worldCamera = Camera.main;
+    }
+}
