b386ef3eafb9ceceb2917ad0128ccfa3616d6911
Theo <theo.farnole@gmail.com>
2020-05-29 13:22:22 +0200

    Add: [camera] pan on screen edge + zoom

--- Assets/Scripts/Game/Managers/Camera/CameraController.cs
+++ Assets/Scripts/Game/Managers/Camera/CameraController.cs
@@ -1,4 +1,5 @@
-﻿using Sirenix.OdinInspector;
+﻿using Lortedo.Utilities.DataTypes;
+using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,21 +8,73 @@ using UnityEngine.Assertions;
 public class CameraController : MonoBehaviour
 {
     [Required]
-    [SerializeField] private CameraControllerData _data;    
+    [SerializeField] private CameraControllerData _data;
+
+    [SerializeField] private Bounds2D _mapLimitX = new Bounds2D(-100, 100);
+    [SerializeField] private Bounds2D _mapLimitZ = new Bounds2D(-100, 100);
 
     void Update()
     {
-        ProcessMovement(Time.deltaTime);
+        ProcessPanBorderMovement(Time.deltaTime);
         ProcessZoomInput(Time.deltaTime);
     }
 
-    private void ProcessMovement(float deltaTime)
+    void OnDrawGizmosSelected()
     {
-        Assert.IsNotNull(_data, "Please assign a CameraControllerData to camera " + name + ".");
+        // don't draw if we don't have 'zoom bounds'
+        if (_data == null)
+            return;
 
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        var size = new Vector3(
+            _mapLimitX.max - _mapLimitX.min,
+            _data.ZoomBounds.max - _data.ZoomBounds.min,
+            _mapLimitZ.max - _mapLimitZ.min
+        );
 
+        var center = new Vector3(
+            _mapLimitX.min,
+            _data.ZoomBounds.min,
+            _mapLimitZ.min
+        ) + size / 2;
+
+        Bounds bounds = new Bounds(size, center);
+
+        Gizmos.DrawWireCube(center, size);
+    }
+
+    private void ProcessPanBorderMovement(float deltaTime)
+    {
+        Vector3 deltaPosition = Vector3.zero;
+
+        if (Input.GetKey(_data.KeyPanForward) || Input.GetKey(_data.KeyPanForwardAlternative) || Input.mousePosition.y >= Screen.height - _data.PanBorderThickness)
+        {
+            // go forward
+            deltaPosition.z += _data.PanSpeed * deltaTime;
+        }
+
+        if (Input.GetKey(_data.KeyPanBackward) || Input.GetKey(_data.KeyPanBackwardAlternative) || Input.mousePosition.y < 0 + _data.PanBorderThickness)
+        {
+            // go backward
+            deltaPosition.z -= _data.PanSpeed * deltaTime;
+        }
+
+        if (Input.GetKey(_data.KeyPanLeft) || Input.GetKey(_data.KeyPanLeftAlternative) || Input.mousePosition.x < 0 + _data.PanBorderThickness)
+        {
+            // go left
+            deltaPosition.x -= _data.PanSpeed * deltaTime;
+        }
+
+        if (Input.GetKey(_data.KeyPanRight) || Input.GetKey(_data.KeyPanRightAlternative) || Input.mousePosition.x > Screen.width - _data.PanBorderThickness)
+        {
+            // go right
+            deltaPosition.x += _data.PanSpeed * deltaTime;
+        }
+               
+        Translate(deltaPosition);
+    }
+
+    void Translate(Vector3 deltaPosition)
+    {       
         Vector3 forward = transform.forward;
         Vector3 right = transform.right;
 
@@ -29,10 +82,22 @@ public class CameraController : MonoBehaviour
         forward.y = 0;
         right.y = 0;
 
-        Vector3 deltaForward = _data.Speed * deltaTime * vertical * forward;
-        Vector3 deltaRight = _data.Speed * deltaTime * horizontal * right;
+        Vector3 deltaForward = deltaPosition.z * forward;
+        Vector3 deltaRight = deltaPosition.x * right;
 
-        transform.position += deltaForward + deltaRight;
+        Vector3 finalDelta = deltaForward + deltaRight;
+
+        Vector3 finalPosition = transform.position + finalDelta;
+
+        finalPosition.x = Mathf.Clamp(finalPosition.x, _mapLimitX.min, _mapLimitX.max);
+        finalPosition.z = Mathf.Clamp(finalPosition.z, _mapLimitZ.min, _mapLimitZ.max);
+
+        transform.position = finalPosition;
+
+        // if value is inside of bounds, Mathf Clamp doesn't change the 'value' argument's value.
+        // However, if it changed it, it's mean that the camera is out of the map.
+        Assert.AreEqual(transform.position.x, Mathf.Clamp(transform.position.x, _mapLimitX.min, _mapLimitX.max), "Camera out of map limits");
+        Assert.AreEqual(transform.position.z, Mathf.Clamp(transform.position.z, _mapLimitZ.min, _mapLimitZ.max), "Camera out of map limits");
     }
 
     private void ProcessZoomInput(float deltaTime)
--- Assets/Scripts/Game/Managers/Camera/CameraControllerData.cs
+++ Assets/Scripts/Game/Managers/Camera/CameraControllerData.cs
@@ -6,14 +6,37 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Tartaros/Camera Controller")]
 public class CameraControllerData : ScriptableObject
 {
-    [SerializeField] private float _speed = 3;
+    [Header("PAN SPEED")]
+    [SerializeField] private float _panSpeed = 3;
+    [SerializeField] private float _panBorderThickness = 10;
+
+    [Header("PAN SPEED > INPUTS")]
+    [SerializeField] private KeyCode _keyPanForward = KeyCode.Z;
+    [SerializeField] private KeyCode _keyPanBackward = KeyCode.S;
+    [SerializeField] private KeyCode _keyPanLeft = KeyCode.Q;
+    [SerializeField] private KeyCode _keyPanRight = KeyCode.D;
+    [Space]
+    [SerializeField] private KeyCode _keyPanForwardAlternative = KeyCode.UpArrow;
+    [SerializeField] private KeyCode _keyPanBackwardAlternative = KeyCode.DownArrow;
+    [SerializeField] private KeyCode _keyPanLeftAlternative = KeyCode.LeftArrow;
+    [SerializeField] private KeyCode _keyPanRightAlternative = KeyCode.RightArrow;
 
     [Header("ZOOM")]
-    [SerializeField] private float _zoomSpeed = 5;    
+    [SerializeField] private float _zoomSpeed = 5;
     [SerializeField] private Bounds2D _zoomBounds = new Bounds2D(5, 10);
 
-    public float Speed { get => _speed; }
+    public float PanSpeed { get => _panSpeed; }
     public float ZoomSpeed { get => _zoomSpeed; }
+    public float PanBorderThickness { get => _panBorderThickness; }
+    public KeyCode KeyPanForward { get => _keyPanForward; }
+    public KeyCode KeyPanBackward { get => _keyPanBackward; }
+    public KeyCode KeyPanLeft { get => _keyPanLeft; }
+    public KeyCode KeyPanRight { get => _keyPanRight; }
+    public KeyCode KeyPanForwardAlternative { get => _keyPanForwardAlternative; }
+    public KeyCode KeyPanBackwardAlternative { get => _keyPanBackwardAlternative; }
+    public KeyCode KeyPanLeftAlternative { get => _keyPanLeftAlternative; }
+    public KeyCode KeyPanRightAlternative { get => _keyPanRightAlternative; }
+    public Bounds2D ZoomBounds { get => _zoomBounds; }
 
     public float GetClampedZoomPositionDelta(float deltaY, float currentPositionY)
     {
