c0aec0bea6a2dcccee6a572bbed2acf4c8eb0f29
Theo <theo.farnole@gmail.com>
2020-06-08 12:40:49 +0200

    Add: [utilities assertations] custom assert class w/ no exception

---
+++ Assets/Scripts/Utilities/Specific/Assertations/Assert.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TF.Assertations
+{
+    public static class Assert
+    {
+        public static void AreEquals(object expected, object current, string message)
+        {
+#if DEVELOPMENT_BUILD || UNITY_EDITOR
+            if (expected != current)
+            {
+                Debug.LogErrorFormat("Assert : " + message);
+            }
+#endif
+        }
+
+        public static void IsTrue(bool expected, string message)
+        {
+#if DEVELOPMENT_BUILD || UNITY_EDITOR
+            if (!expected)
+            {
+                Debug.LogError("Assert : " + message);
+            }
+#endif
+        }
+
+        public static void IsTrue(bool expected, string message, params string[] args)
+        {
+#if DEVELOPMENT_BUILD || UNITY_EDITOR
+            if (!expected)
+            {
+                Debug.LogErrorFormat("Assert : " + message, args);
+            }
+#endif
+        }
+    }
+}
