283aaea622072a73fbce18997a8a2ea59849f5c4
Theo <theo.farnole@gmail.com>
2020-06-16 12:11:11 +0200

    Add: [camera] raycast AudioListener to the ground

---
+++ Assets/Scripts/Utilities/Specific/CommentComponent.cs
@@ -0,0 +1,12 @@
+﻿namespace Game.Utils
+{
+    using Sirenix.OdinInspector;
+    using UnityEngine;
+
+    public class CommentComponent : MonoBehaviour
+    {
+        [TextArea]
+        [DrawWithUnity]
+        [SerializeField] private string _notes = "Comment Here.";                                              
+    }
+}
