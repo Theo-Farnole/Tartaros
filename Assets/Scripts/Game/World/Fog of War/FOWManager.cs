8bedd40ff48fad23bd92079be26c618476367a7a
Theo <theo.farnole@gmail.com>
2020-06-17 21:04:47 +0200

    Fix + Readability: [fow manager] errors preventing buiding + #region Scope instead of behavior

--- Assets/Scripts/Game/World/Fog of War/FOWManager.cs
+++ Assets/Scripts/Game/World/Fog of War/FOWManager.cs
@@ -1,14 +1,12 @@
-﻿using Game.Entities;
-using Lortedo.Utilities.Pattern;
-using System.Collections;
-using System.Collections.Generic;
-using System.Text;
-using UnityEngine;
-using UnityEngine.Assertions;
-
-namespace Game.FogOfWar
+﻿namespace Game.FogOfWar
 {
-    public class FOWManager : Singleton<FOWManager>
+    using Game.Entities;
+    using Lortedo.Utilities.Pattern;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.Assertions;
+
+    public partial class FOWManager : Singleton<FOWManager>
     {
         #region Fields
         private const string debugLogHeader = "FOWManager : ";
@@ -45,15 +43,9 @@ namespace Game.FogOfWar
             UpdateVisibilityMap();
             UpdateCoverablesVisibility();
         }
-
-        void OnDrawGizmos()
-        {
-            if (_debugDrawSnapGrid)
-                _snapGrid?.DrawGizmos();
-        }
         #endregion
 
-        #region Entities Manager
+        #region Public Methods
         public void AddViewer(EntityFogVision entity)
         {
             _viewers.Add(entity);
@@ -73,27 +65,13 @@ namespace Game.FogOfWar
         {
             _coverables.Add(entity);
         }
-        #endregion
 
-        #region Get Tiles Methods
-        private Vector2Int CoordsToUncenteredCoords(Vector2Int centeredCoords, Vector2Int size)
-        {
-            if (size.x % 2 != 0) size.x--;
-            if (size.y % 2 != 0) size.y--;
 
-            Vector2Int uncenteredCoords = centeredCoords - size / 2;
+        public bool AreTilesVisible(Vector3 position, Vector2Int size) => AreTilesVisible(WorldToCoords(position), size);
 
-            return uncenteredCoords;
-        }
+        public Vector2Int WorldToCoords(Vector3 position) => _snapGrid.WorldToCoords(position);
 
-        public bool AreTilesVisible(Vector3 position, Vector2Int size)
-            => AreTilesVisible(WorldToCoords(position), size);
-
-        public Vector2Int WorldToCoords(Vector3 position)
-            => _snapGrid.WorldToCoords(position);
-
-        public Vector3 CoordsToWorld(Vector2Int coords)
-            => _snapGrid.CoordsToWorldPosition(coords);
+        public Vector3 CoordsToWorld(Vector2Int coords) => _snapGrid.CoordsToWorldPosition(coords);
 
         public bool AreTilesVisible(Vector2Int coords, Vector2Int size)
         {
@@ -117,29 +95,11 @@ namespace Game.FogOfWar
             return true;
         }
 
-        public bool TryGetTile(Vector3 worldPosition, out FogState fogState)
-            => TryGetTile(_snapGrid.WorldToCoords(worldPosition), out fogState);
-
-        private bool TryGetTile(Vector2Int coords, out FogState fogState)
-        {
-            if (coords.x < 0 || coords.x >= _fogMap.Size || coords.y < 0 || coords.y >= _fogMap.Size)
-            {
-                Debug.LogErrorFormat(debugLogHeader + "Coords passed in args aren't in visibility map");
-                fogState = FogState.Visible;
-                return false;
-            }
-
-            fogState = _fogMap.GetValue(coords.x, coords.y);
-
-            // overwrite output
-            if (_isDisabled) fogState = FogState.Visible;
-
-            return true;
-        }
+        public bool TryGetTile(Vector3 worldPosition, out FogState fogState) => TryGetTile(_snapGrid.WorldToCoords(worldPosition), out fogState);
         #endregion
 
-        #region Visibility Map Routine Methods
-        void UpdateVisibilityMap()
+        #region Private Methods
+        private void UpdateVisibilityMap()
         {
             if (_isDisabled)
                 return;
@@ -148,7 +108,7 @@ namespace Game.FogOfWar
             _fogMap.DrawViewersVision(_viewers, _snapGrid);
         }
 
-        void UpdateCoverablesVisibility()
+        private void UpdateCoverablesVisibility()
         {
             if (_isDisabled)
                 return;
@@ -171,9 +131,8 @@ namespace Game.FogOfWar
                 _coverables[i].IsCover = isCover;
             }
         }
-        #endregion
 
-        void DisableFOW()
+        private void DisableFOW()
         {
             _isDisabled = true;
             UncoverAllCoverables();
@@ -181,7 +140,7 @@ namespace Game.FogOfWar
             _projectorFogOfWar.enabled = false;
         }
 
-        void UncoverAllCoverables()
+        private void UncoverAllCoverables()
         {
             foreach (var coverable in _coverables)
             {
@@ -189,11 +148,51 @@ namespace Game.FogOfWar
             }
         }
 
-        void ReactiveFOW()
+        private void ReactiveFOW()
         {
             _isDisabled = false;
             _projectorFogOfWar.enabled = true;
         }
+
+        private Vector2Int CoordsToUncenteredCoords(Vector2Int centeredCoords, Vector2Int size)
+        {
+            if (size.x % 2 != 0) size.x--;
+            if (size.y % 2 != 0) size.y--;
+
+            Vector2Int uncenteredCoords = centeredCoords - size / 2;
+
+            return uncenteredCoords;
+        }
+
+
+        private bool TryGetTile(Vector2Int coords, out FogState fogState)
+        {
+            if (coords.x < 0 || coords.x >= _fogMap.Size || coords.y < 0 || coords.y >= _fogMap.Size)
+            {
+                Debug.LogErrorFormat(debugLogHeader + "Coords passed in args aren't in visibility map");
+                fogState = FogState.Visible;
+                return false;
+            }
+
+            fogState = _fogMap.GetValue(coords.x, coords.y);
+
+            // overwrite output
+            if (_isDisabled) fogState = FogState.Visible;
+
+            return true;
+        }
+        #endregion
         #endregion
     }
+
+#if UNITY_EDITOR
+    public partial class FOWManager : Singleton<FOWManager>
+    {
+        void OnDrawGizmos()
+        {
+            if (_debugDrawSnapGrid)
+                _snapGrid?.DrawGizmos();
+        }
+    }
+#endif
 }
