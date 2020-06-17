e5c093de601d26749ebe6953ae6f0831d4e2eb28
Theo <theo.farnole@gmail.com>
2020-06-17 09:47:02 +0200

    Implement: [sounds] SFX_Interface_ButtonPressed

---
+++ Assets/Scripts/Game/Audio/IgnoreAudioListenerPause.cs
@@ -0,0 +1,21 @@
+﻿namespace Game.Audio
+{
+    using UnityEngine;
+
+    [RequireComponent(typeof(AudioSource))]
+    public class IgnoreAudioListenerPause : MonoBehaviour
+    {
+        [SerializeField] private bool _ignoreListenerPause = true;
+        [SerializeField] private bool _destroyScripAfterIgnoring = true;
+
+        void Start()
+        {
+            GetComponent<AudioSource>().ignoreListenerPause = _ignoreListenerPause;
+
+            if (_destroyScripAfterIgnoring)
+            {
+                Destroy(this);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Audio/PlaySoundOnButtonClick.cs
@@ -0,0 +1,41 @@
+﻿namespace Game.Audio
+{
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    [RequireComponent(typeof(Button))]
+    public class PlaySoundOnButtonClick : MonoBehaviour
+    {
+        [SerializeField] private Sound2D _soundToPlayOnClick = Sound2D.OnButtonClick;
+
+        private Button _button;
+        private static AudioManager2D _audioManager2D;
+
+        private Button Button
+        {
+            get
+            {
+                if (_button == null)
+                    _button = GetComponent<Button>();
+
+                return _button;
+            }
+        }
+
+        private static AudioManager2D AudioManager
+        {
+            get
+            {
+                if (_audioManager2D == null)
+                    _audioManager2D = FindObjectOfType<AudioManager2D>();
+
+                return _audioManager2D;
+            }
+        }
+
+        void OnEnable()
+        {
+            Button.onClick.AddListener(() => AudioManager.PlayOneShotRandomClip(Sound2D.OnButtonClick));
+        }
+    }
+}
