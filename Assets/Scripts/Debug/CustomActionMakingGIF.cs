083f21f77efcb78e9eb457c931aca96f7aa19f9a
Theo <theo.farnole@gmail.com>
2020-06-19 11:47:07 +0200

    Add: [scene] SC_Video_Battle

---
+++ Assets/Scripts/Debug/CustomActionMakingGIF.cs
@@ -0,0 +1,63 @@
+﻿using Game.Entities;
+using Game.Entities.Actions;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomActionMakingGIF : MonoBehaviour
+{
+    [SerializeField] private float _destinationOffsetZ = 10;
+    [SerializeField] private KeyCode _input = KeyCode.E;
+    [SerializeField] private bool _playOnStart = true;
+
+    private void Start()
+    {
+        if (_playOnStart)
+        {
+            MakeEntitiesGreatAgain();
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(_input))
+        {
+            MakeEntitiesGreatAgain();
+        }
+    }
+
+    private void MakeEntitiesGreatAgain()
+    {
+        Debug.Log("MakeEntitiesGreatAgain, attack'll work at " + System.DateTime.Now.AddSeconds(ActionMoveToPositionAggressively.DELAY).ToLongTimeString());
+
+        Entity[] entities = FindObjectsOfType<Entity>();
+
+        int length = entities.Length;
+        for (int i = 0; i < length; i++)
+        {
+            Entity entity = entities[i];
+
+            // calculate position
+            Vector3 position = entity.transform.position + GetOffset(entity.Team);
+
+            // apply move to position method
+            ActionMoveToPositionAggressively action = new ActionMoveToPositionAggressively(entity, position);
+            entity.SetAction(action);
+        }
+    }
+
+    Vector3 GetOffset(Team team)
+    {
+        switch (team)
+        {
+            case Team.Player:
+                return _destinationOffsetZ * Vector3.forward;
+
+            case Team.Enemy:
+                return -_destinationOffsetZ * Vector3.forward;
+
+            default:
+                throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Scripts/Debug/SetChildsEntitiesTeam.cs
@@ -0,0 +1,30 @@
+﻿using Game.Entities;
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetChildsEntitiesTeam : MonoBehaviour
+{
+    [SerializeField] private Team _wantedTeam;
+    [SerializeField] private bool _setAllOnAwake = true;
+
+    private void Awake()
+    {
+        if (_setAllOnAwake)
+            SetAll();
+    }
+
+    [Button]
+    void SetAll()
+    {
+        Entity[] entities = GetComponentsInChildren<Entity>();
+
+        for (int i = 0; i < entities.Length; i++)
+        {
+            entities[i].Team = _wantedTeam;
+        }
+
+        Debug.LogFormat("Set all of {0} entities.", entities.Length);
+    }
+}
